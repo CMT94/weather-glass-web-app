b3273cf4285c963b56f370227f2d0f423989f9de
Clément M <cmt94.dev@gmail.com>
2023-10-16 19:56:17 +0200

    async func getSearchOptions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 
-import { BiSearch } from "react-icons/bi";
 import SearchForm from "./components/Icons/SearchForm";
 
 const App = (): JSX.Element => {
-  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
   const [searchInputValue, setSearchInputValue] = React.useState<string>("");
+
+  const getSearchOptions = async (search: string) => {
+    if (search.length > 0) {
+      fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${search.trim()}&limit=5&appid=${
+          process.env.REACT_APP_API_KEY
+        }`
+      );
+    }
+  };
+
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInputValue(e.target.value);
+
+    getSearchOptions(e.target.value);
   };
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
