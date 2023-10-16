2aa984e9c3a9921f0285e1e5d223288cf2bb55a7
Clément M <cmt94.dev@gmail.com>
2023-10-16 17:45:45 +0200

    change ts config file

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ const App = () => {
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
       Hello world
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
