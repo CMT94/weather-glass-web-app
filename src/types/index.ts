717cf2335140386839a7f0d47c8d3982d32036ad
Clément M <cmt94.dev@gmail.com>
2023-10-18 13:10:28 +0200

    Forecast type TS

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,43 @@ export interface SearchOption {
   name: string;
   state: string;
 }
+
+export interface Forecast {
+  base: string;
+  clouds: Record<string, number>;
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  dt: number;
+  id: number;
+  main: {
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  name: string;
+  rain: Record<string, number>;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  visibility: number;
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+  };
+}
