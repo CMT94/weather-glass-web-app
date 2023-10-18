75986b02bc9c8c88a353f53b6da3aa73937eafe2
Clément M <cmt94.dev@gmail.com>
2023-10-18 11:07:39 +0200

    refactor in SearchSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import SearchForm from "./components/Icons/SearchForm";
+import SearchSection from "./components/SearchSection";
 import { SearchOption } from "./types";
 
 const App = (): JSX.Element => {
@@ -52,28 +52,14 @@ const App = (): JSX.Element => {
   }, [selectedCity]);
 
   return (
-    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
-        <h1 className="text-4xl font-thin">
-          Weather <span className="font-black">Forecast</span>
-        </h1>
-        <p className="text-sm mt-2">
-          Enter below a place you want to know the weather and select an option
-          from the dropdown
-        </p>
-
-        <div className="relative flex mt-10 md:mt-4">
-          <SearchForm
-            inputValue={searchInputValue}
-            options={searchOptions}
-            selectedCity={selectedCity}
-            updateSelectedCity={setSelectedCity}
-            onChangeHandler={handleChangeInput}
-            onSubmitHandler={handleSubmit}
-          />
-        </div>
-      </section>
-    </main>
+    <SearchSection
+      searchInputValue={searchInputValue}
+      searchOptions={searchOptions}
+      selectedCity={selectedCity}
+      updateSelectedCity={setSelectedCity}
+      handleChangeInput={handleChangeInput}
+      handleSubmit={handleSubmit}
+    />
   );
 };
 
